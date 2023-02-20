0b2e9c88a7c3f7711add35dcd03694ab59894a52
haoguoxin <haoguoxin@ainirobot.com>
2023-02-20 11:39:02 +0800

    豹小秘Plus大屏展示

---
+++ 大屏显示/app/demo/DemoScreen.tsx
@@ -0,0 +1,97 @@
+import { BaseComponent, triggerManager, BaseComponentProps, ExDisplay } from 'orionos-eve-core';
+import React from 'react';
+import { observer } from 'mobx-react';
+import { Text, View, StyleSheet, Dimensions, TouchableOpacity, Button, ImageBackground, Image } from 'react-native';
+import { DemoViewModel } from './DemoViewModel';
+import { DemoVoice } from './DemoVoice';
+import { DemoTrigger } from './DemoTrigger';
+import { demoModel } from './DemoModel';
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#f3eae8',
+        // width: Dimensions.get( 'window').width,
+        // height: Dimensions.get( 'window').height,
+        width: 1200,
+        height: 1920,
+        opacity: 1
+    },
+    buttons: {
+        width: 200,
+        height: 30,
+        marginTop: 10,
+        backgroundColor: 'white',
+        borderRadius: 2.3,
+        textAlign: 'center',
+        textAlignVertical: 'center',
+    }
+});
+
+//注册trigger跳转，必须添加，否则trigger无效
+triggerManager.addTrigger(new DemoTrigger());
+
+/**
+ * 功能UI界面
+ */
+@observer
+export class DemoScreen extends BaseComponent<BaseComponentProps, DemoViewModel, DemoVoice> {
+
+    public viewModel: DemoViewModel;
+
+    public constructor(props: BaseComponentProps) {
+        super(props);
+
+        this.viewModel = new DemoViewModel();
+        let voice = new DemoVoice(this.viewModel);
+
+        //关联ViewModel及Voice的生命周期到当前界面上
+        this.setViewModel(this.viewModel);
+        this.setVoice(voice);
+    }
+
+    public componentDidMount() {
+        //重写界面的didMount，必须调用super
+        super.componentDidMount();
+    }
+
+    public componentWillMount() {
+
+    }
+
+    public componentWillUnmount() {
+        //重写界面的Unmount，必须调用super
+        super.componentWillUnmount();
+    }
+
+    public render() {
+        return (
+            <>
+                <ImageBackground
+                    style={{
+                        width: '100%',
+                        height: '100%',
+                        alignItems: "center",
+                        justifyContent: "center",
+                        backgroundColor: "#FF0000FF"
+                    }}
+                    source={require('../../img/bg.png')}
+                >
+                <ExDisplay mode="multi">
+                    <View>
+                        <View style={{ width: '100%', height: '100%', backgroundColor: 'green' }}>
+                           <Text>
+                            大屏展示内容
+                           </Text>
+                        </View>
+                    </View>
+                </ExDisplay>                  
+                </ImageBackground>
+            </>
+        );
+
+    }
+
+    public triggerWakeup = () => {
+        this.viewModel.exit();
+    }
+}
